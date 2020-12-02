4178b70f433fe52112db6022481c57efd39e02a3
NasirAhmed <71327008+Nasir-ahmed66@users.noreply.github.com>
2020-12-03 01:00:31 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+const days  = document.querySelector('#days');
+const hours  = document.querySelector('#hours');
+const mins  = document.querySelector('#mins');
+const seconds  = document.querySelector('#seconds');
+const newYear = '1 Jan 2021';
+
+
+function countTime(){
+    //new years date
+    const newYearsDate  = new Date(newYear);
+    //Current Date
+    const currentDate  = new Date();
+    const remainingDate = new Date(newYearsDate -currentDate );  
+    days.innerHTML  = remainingDate.getDate();
+    hours.innerHTML  = remainingDate.getHours();
+    mins.innerHTML  = remainingDate.getMinutes()
+    seconds.innerHTML  =remainingDate.getSeconds();
+}
+countTime();
+
+setInterval(countTime , 1000);
+
+
