8043bceeca45c7910013f2a20f9e63eb93f39f11
NasirAhmed <71327008+nasirahmedd@users.noreply.github.com>
2021-07-27 14:42:55 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const days  = document.querySelector('#days');
 const hours  = document.querySelector('#hours');
 const mins  = document.querySelector('#mins');
 const seconds  = document.querySelector('#seconds');
-const newYear = '1 Jan 2021';
+const newYear = '1 Jan 2022';
 
 
 function countTime(){
